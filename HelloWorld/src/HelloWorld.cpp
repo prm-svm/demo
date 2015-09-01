8223f755d945ca0d21a2378622754c5ea2f96af9
prm-svm <prm.svm@gmail.com>
2015-09-01 11:30:54 +0530

    source code

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Paramasivam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	cout << "It's alive" << endl;
+	return 0;
+}
